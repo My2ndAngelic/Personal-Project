fc98bcd48f31a2d3982c7179e3c42672fea379f4
My2ndAngelic <m2a@my2ndangelic.com>
2024-12-02 13:58:06 +0100

    Update

--- Java/src/main/java/aoc/AoC2024/AoC1.java
+++ Java/src/main/java/aoc/AoC2024/AoC1.java
@@ -3,6 +3,7 @@ package aoc.AoC2024;
 import aoc.AoCUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,14 +13,16 @@ public class AoC1 {
         String directory = String.format("%s/src/main/java/aoc/AoC2024/input/input1.txt", System. getProperty("user.dir"));
         try {
 //            List<String> output = AoCUtilities.fileImportToStringArrayList(String.format("%s/src/main/java/aoc/AoC2024/input/input1.txt", System. getProperty("user.dir")));
-            List<String> output = AoCUtils.fileImportToGeneric(directory, String::valueOf);
-            List<List<Integer>> output2 = output.stream()
+            List<String> output = AoCUtils.fileImportToGeneric(directory, String::valueOf, ArrayList.class);
+//            String[] ooutput = AoCUtils.fileImportToGeneric(directory, String::valueOf, ArrayList.class).toArray(new String[0]);
+            ArrayList<List<Integer>> output2 = (ArrayList<List<Integer>>) output.stream()
                     .map(s -> Arrays.stream(s.split("\\s+"))
                             .map(Integer::parseInt)
                             .collect(Collectors.toList()))
                     .collect(Collectors.toList());
 
             System.out.println(output2);
+//            System.out.println(Arrays.toString(ooutput));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- Java/src/main/java/aoc/AoCUtils.java
+++ Java/src/main/java/aoc/AoCUtils.java
@@ -3,19 +3,27 @@ package aoc;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
 
 public class AoCUtils {
-    public static <T> List<T> fileImportToGeneric(String path, Function<String, T> mapper) throws FileNotFoundException {
+    public static <ElementType, ListType> List<ElementType> fileImportToGeneric(String path, Function<String, ElementType> mapper   , Class<ListType> listType) throws FileNotFoundException {
         File myFile = new File(path);
         Scanner sc = new Scanner(myFile);
-        List<T> myData = new ArrayList<T>();
+        List<ElementType> myData;
+        if (listType.equals(ArrayList.class)) {
+            myData = new ArrayList<>();
+        } else if (listType.equals(LinkedList.class)) {
+            myData = new LinkedList<>();
+        } else { // Handle other types or throw an exception
+            throw new IllegalArgumentException("Unsupported listType: " + listType);
+        }
 
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
-            T value = mapper.apply(line);
+            ElementType value = mapper.apply(line);
             myData.add(value);
         }
 
